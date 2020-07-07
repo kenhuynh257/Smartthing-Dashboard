157a7ad9bfeb7a99a0b0c81030ecee764ef4fe02
Hung Huynh <huynhh1@seattleu.edu>
2020-07-07 12:58:00 -0700

    Initial commit

---
+++ src/main/java/com/smartthing/SmartthingKeypad/SmartthingKeypadApplication.java
@@ -0,0 +1,13 @@
+package com.smartthing.SmartthingKeypad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartthingKeypadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartthingKeypadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartthing/SmartthingKeypad/SmartthingKeypadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartthing.SmartthingKeypad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartthingKeypadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
