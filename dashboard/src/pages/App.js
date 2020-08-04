09c24d686ec5d82750c426b16d405e5b794ccb5a
Hung Huynh <huynhh1@seattleu.edu>
2020-08-03 23:22:46 -0700

    move axios method to componentdidmount

--- dashboard/src/pages/App.js
+++ dashboard/src/pages/App.js
@@ -2,32 +2,31 @@ import React from 'react';
 import '../css/App.css';
 import RemoteKey from "./RemoteKey";
 import Boards from "./Boards";
+import axios from 'axios';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {key: null};
-        global.Key = null;
+        this.state = {key: ""};
+    }
+
+    componentDidMount() {
+        axios.get('/RemoteKey/')
+            .then(res => {
+                console.log(res.data);
+                this.setState({key: res.data});
+                //global.Key = res.data;
+
+            })
     }
 
     render() {
-        const axios = require('axios');
 
-        axios.get('/RemoteKey/')
-            .then(res=>{
-                console.log(res.data);
-                //this.setState({key: res.data});
-                global.Key = res.data;
-
-            })
-
-
-        if(global.Key == null){
-            return <RemoteKey />;
-        }
-        else{
-            return <Boards />;
+        if (this.state.key === "") {
+            return <RemoteKey/>;
+        } else {
+            return <Boards/>;
         }
 
 
