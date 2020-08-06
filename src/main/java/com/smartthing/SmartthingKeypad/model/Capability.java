28f5c02d011bdb44f7b82c0ed94f800806331697
Hung Huynh <huynhh1@seattleu.edu>
2020-08-05 17:14:57 -0700

    Add Enum for Capability

---
+++ src/main/java/com/smartthing/SmartthingKeypad/model/Capability.java
@@ -0,0 +1,10 @@
+package com.smartthing.SmartthingKeypad.model;
+
+public enum Capability {
+    SWITCH,
+    WATERSENSOR,
+    ALARM,
+    BATTERY,
+    CONTACTSENSOR,
+    TEMPERATUREMEASUREMENT
+}
