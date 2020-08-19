302d27e5ace2ffe7c15f3d89fe3117b4c084b97c
Hung Huynh <huynhh1@seattleu.edu>
2020-08-19 10:43:23 -0700

    Revert back to just show cards

--- dashboard/src/pages/Boards.js
+++ dashboard/src/pages/Boards.js
@@ -2,18 +2,20 @@ import React from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
+import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 
+
 export default class Boards extends React.Component {
     state = {
-        device: [],
+        device: []
     }
 
     componentDidMount() {
         axios.get('/GetData/GetAllDevices')
             .then(res => {
-                console.log(res.data.items);
+                console.log("res " + JSON.stringify(res.data));
                 this.setState({device: res.data});
 
             })
@@ -23,51 +25,53 @@ export default class Boards extends React.Component {
         console.log(id);
     }
 
+
+
+
     render() {
         let data = this.state.device;
-        let reData = this.state.device.reduce((r,a)=>{
-            r[a.room] = [...r[a.room]||[],a];
-            return r;
-        },{});
-        console.log(reData);
+
+        console.log("data " + JSON.stringify(data));
+
+
+
+        console.log( data);
         const blueStatus = ["on", "dry", "closed"];
         return (
-            <>
-                <CardDeck>
-                    {data.map(s => (
+            <Container>
+                <>
+                    <CardDeck>
+                        {data.map(s => (
 
-                        <Card style={{minWidth: '18rem', maxWidth: '18rem'}}>
-                            <Card.Body>
-                                <Card.Title>{s.label}</Card.Title>
-                            </Card.Body>
-                            {
-                                s.capabilities.map(v => {
-                                    return (
-                                        <ListGroup className="list-group-flush">
-                                            <ListGroupItem variant={blueStatus.includes(Object.values(v)[0])  ? 'primary' : 'danger'}
-                                                           action={true}
-                                                           onClick={()=>this.handleSubmit(Object.values(v)[0])}>
-                                                {Object.keys(v)[0]}: {Object.values(v)[0]}
-                                            </ListGroupItem>
-                                        </ListGroup>
-                                    )
-                                })
-                            }
+                            <Card style={{minWidth: '18rem', maxWidth: '18rem'}}>
+                                <Card.Body>
+                                    <Card.Title>{s.label}</Card.Title>
+                                </Card.Body>
+                                {
+                                    s.capabilities.map(v => {
+                                        return (
+                                            <ListGroup className="list-group-flush">
+                                                <ListGroupItem variant={blueStatus.includes(Object.values(v)[0])  ? 'primary' : 'danger'}
+                                                               action={true}
+                                                               onClick={()=>this.handleSubmit(Object.values(v)[0])}>
+                                                    {Object.keys(v)[0]}: {Object.values(v)[0]}
+                                                </ListGroupItem>
+                                            </ListGroup>
+                                        )
+                                    })
+                                }
 
-                        </Card>
+                            </Card>
 
-                    ))}
-                </CardDeck>
+                        ))}
+                    </CardDeck>
 
 
-            </>
+                </>
+
+            </Container>
 
 
         );
-
     }
-
-
 }
-
-//
