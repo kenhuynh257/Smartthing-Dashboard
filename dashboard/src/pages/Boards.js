37ce6b7c963bf6f2c04f9caf07f899d418a62fce
Hung Huynh <huynhh1@seattleu.edu>
2020-09-07 15:11:46 -0700

    Add Modal

--- dashboard/src/pages/Boards.js
+++ dashboard/src/pages/Boards.js
@@ -5,12 +5,14 @@ import CardDeck from 'react-bootstrap/CardDeck'
 import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 
 export default class Boards extends React.Component {
     state = {
         device: [],
-        blueStatus : ["on", "dry", "closed"]
+        blueStatus: ["on", "dry", "closed"]
 
     }
 
@@ -18,11 +20,49 @@ export default class Boards extends React.Component {
         axios.get('/GetData/GetAllDevices')
             .then(res => {
                 this.setState({device: res.data});
+
             })
     }
 
+    RenderModal() {
+
+        return (
+            <>
+                <Modal show={true}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Password</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={this.handlePassword}>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="number"/>
+                        </Form>
+                    </Modal.Body>
+
+                </Modal>
+            </>
+        );
+    }
+
+    handlePassword(pass) {
+        console.log(pass);
+    }
+
     handleSubmit(id) {
+        let url = '/CheckDeviceID/' + id;
         console.log(id);
+        axios.get(url)
+            .then(r => {
+                if (r.data === "notDisarm") {
+                    this.RenderModal();
+                } else {
+                    console.log(r.data);
+                }
+
+
+            }, (er) => {
+                console.log(er);
+            });
     }
 
     render() {
@@ -46,8 +86,9 @@ export default class Boards extends React.Component {
                                                 <ListGroupItem
                                                     variant={this.state.blueStatus.includes(Object.values(v)[0]) ? 'primary' : 'danger'}
                                                     action={true}
-                                                    onClick={() => this.handleSubmit(Object.values(v)[0])}>
+                                                    onClick={() => this.handleSubmit(s.deviceId)}>
 
+                                                    {/*capacity:status*/}
                                                     {Object.keys(v)[0]}: {Object.values(v)[0]}
                                                 </ListGroupItem>
                                             </ListGroup>
